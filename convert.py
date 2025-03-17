d029e2c0a80942396a89ef81b07f043f46023d00
Ronald-Contijo <ronaldques1@gmail.com>
2025-03-17 09:43:30 -0300

    Adicionados comentários
    Explicações sobre estrutura de pastas

--- convert.py
+++ convert.py
@@ -2,13 +2,23 @@ import os
 import img2pdf
 from PyPDF2 import PdfMerger
 
-# Diretório base onde estão as pastas
+# Diretório base onde estão os mangas (Diretório padrão do HakuNeko Desktop)
 DIRETORIO_BASE = "/home/ronald/Documentos/Mangas"
+
+'''
+Estrutura de pastas 
+/home/ronald/Documentos/Mangas
+---/home/ronald/Documentos/Mangas/Manga1/
+------/home/ronald/Documentos/Mangas/Manga1/Capitulo1
+------/home/ronald/Documentos/Mangas/Manga1/Capitulo2
+
+'''
+
 
 # Lista de extensões de imagem suportadas
 EXTENSOES = (".jpg", ".jpeg", ".png")
 
-# Função para converter imagens de uma pasta em PDF
+
 def converter_pasta_para_pdf(caminho_pasta, nome_pdf):
     imagens = []
     
@@ -17,7 +27,6 @@ def converter_pasta_para_pdf(caminho_pasta, nome_pdf):
         if arquivo.lower().endswith(EXTENSOES):
             imagens.append(os.path.join(caminho_pasta, arquivo))
 
-    # Se houver imagens, cria o PDF
     if imagens:
         caminho_pdf = os.path.join(caminho_pasta, f"{nome_pdf}.pdf")
         with open(caminho_pdf, "wb") as pdf:
@@ -34,7 +43,7 @@ def processar_pasta_principal(pasta_principal):
     
     pdfs_gerados = []
     
-    # Percorre as subpastas dentro da pasta principal
+
     for subpasta in sorted(os.listdir(caminho_principal)):
         caminho_subpasta = os.path.join(caminho_principal, subpasta)
         
@@ -44,7 +53,7 @@ def processar_pasta_principal(pasta_principal):
             if pdf_gerado:
                 pdfs_gerados.append(pdf_gerado)
     
-    # Juntar PDFs de 20 em 20
+    # Juntar PDFs de 20 em 20 pra ficar fácil de ler no KIndle
     for i in range(0, len(pdfs_gerados), 20):
         merger = PdfMerger()
         subset_pdfs = pdfs_gerados[i:i+20]
